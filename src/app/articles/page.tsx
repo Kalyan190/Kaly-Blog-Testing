c9d43b7b886cb42fda6f95977ef6b1244f546fe9
Kalyan Kumar <kalyaniit795@gmail.com>
2025-04-18 14:22:14 +0530

    error occur in deployment searchParams

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -30,7 +30,9 @@ const page :React.FC<SearchPageProps> = async({searchParams}) => {
       <main className='container mx-auto px-4 py-12 sm:px-6 lg:text-5xl'>
           <div className='mb-12 space-y-6 text-center'>
              <h1 className='text-4xl font-bold sm:text-5xl'>All Articles</h1>
-             <ArticleSearchInput/>
+             <Suspense>
+                 <ArticleSearchInput />
+             </Suspense>
           </div>
           {/* All Articles Page */}
           <Suspense fallback={<AllArticlesPageSkeleton/>}>
